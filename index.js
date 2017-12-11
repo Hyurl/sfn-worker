30a17d3b447453bcb24cf1ddac1465d0066e74cd
hyurl <i@hyurl.com>
2017-12-11 19:21:49 +0800

    Fix some bugs.

--- index.js
+++ index.js
@@ -199,7 +199,11 @@ class Worker extends EventEmitter {
             if (cb instanceof Function) {
                 this.once("----get-workers----", workers => {
                     for (let i in workers) {
-                        workers[i] = new this.constructor(workers[i].id, workers[i].keepAlive);
+                        if (workers[i].id === this.id){
+                            workers[i] = this;
+                        }else{
+                            workers[i] = new this.constructor(workers[i].id, workers[i].keepAlive);
+                        }
                     }
                     cb(workers);
                 }).emit("----get-workers----");
